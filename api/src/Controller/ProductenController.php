29a4a191db4eab4bd56a3ad2a5709c58bfba47a8
Ruben van der Linde <ruben@conduction.nl>
2020-02-19 08:56:26 +0100

    Voorbeeld bl

--- api/src/Controller/ProductenController.php
+++ api/src/Controller/ProductenController.php
@@ -10,6 +10,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 /**
  * Class ProductController
@@ -23,62 +24,115 @@ class ProductenController extends AbstractController
      * @Route("/")
      * @Template
      */
-	public function indexAction(Request $request, CommonGroundService $commonGroundService)
+	public function indexAction(Session $session, Request $request, CommonGroundService $commonGroundService)
     {
+		// Wat doen we hier eigenlijk met organizations en groups?
     	$organizations = $commonGroundService->getResourceList('https://cc.zaakonline.nl/organizations',["name"=>"fc"]);
     	$groups = $commonGroundService->getResourceList('https://pdc.larping.online/groups',["sourceOrganization"=>"816802828"]);
     	$products = $commonGroundService->getResourceList('https://pdc.larping.online/products?sourceOrganization=816802828');
     	
+    	$orderUri = $session->get('order');
+    	$order = false; // Aangezien we de order variable aan hettemplate passeeren moetdie zowiezo bestaan
+    	if($orderUri){
+    		$order = $commonGroundService->getResource($orderUri);
+    	}
+    	
+    	
     	// Kijken of het formulier is getriggerd
     	if ($request->isMethod('POST')){
-    		// kijken of er in de sessie al een order zit, zo nee order aan maken
+    		// kijken of er in de sessie al een order zit, zo nee order aan maken. We slaan hier alleen de order ID (URI) op. Het bijhouden van het order object laten we via de commonground controller aan de cache
+    		if(!$orderUri){
+    			
+    			$order = [];
+    			$order[''] = ''; // etc
+    			    			
+    			$order = $commonGroundService->getCreateResource($order, 'https://orc.larping.online/orders');
+    			$orderUri = $order['@id'];
+    			$session->set('order', $orderUri);    			
+    		}
     		
     		// order regel aanmaken op order met de gevraagde gegevens
     		
-    		// flashban zetten met eindresultaat
+    		$orderline = [];
+    		$orderline['order'] = $order['@id']; // de vraag is of we hier de baseurl zouden moeten strippen via php pathinfo
+    		$orderline[''] = ''; // etc
+    		
+    		$orderline= $commonGroundService->getCreateResource($orderline, 'https://orc.larping.online/orderlines'); // diedit uit mijn hoofd dus weet niet of het werkelijk order linses is
+    		
+    		// Omdat we een order line hebben toegeveogd willen we het order opnieuw ophalen EN een cash refresh afdwingen
+    		$order = $commonGroundService->getResource($orderUri, true);
+    		
+    		// flashban zetten met eindresultaat    		
+    		$this->addFlash('success', 'Uw product is toegevoegd');
     	}
     	
-    	return ['organisations'=>$organizations,'groups'=>$groups,'products'=> $products, $this->redirect('producten')];
+    	return ['organisations'=>$organizations,'groups'=>$groups,'products'=> $products,'order'=> $order, $this->redirect('producten')];
     }
 
     /**
      * @Route("/betalen")
      * @Template
      */
-	public function betalenAction(Request $request, CommonGroundService $commonGroundService)
+    public function betalenAction(Session $session, Request $request, CommonGroundService $commonGroundService)
     {
+    	// Wat doen we hier  eigenlijk met organisations en groups?
     	$organizations = $commonGroundService->getResourceList('https://cc.zaakonline.nl/organizations',["name"=>"fc"]);
     	$groups = $commonGroundService->getResourceList('https://pdc.larping.online/groups',["sourceOrganization"=>"816802828"]);
+    	
+    	$orderUri = $session->get('order');
+    	$order = false; // Aangezien we de order variable aan hettemplate passeeren moetdie zowiezo bestaan
+    	if($orderUri){
+    		$order = $commonGroundService->getResource($orderUri);
+    	}
     	
     	// Kijken of het formulier is getriggerd
     	if ($request->isMethod('POST')){
     		// contact persoon aanmaken op order
+    		$contact = [];    		
+    		$contact[''] = ''; // etc    	
     		
-    		// order updaten
+    		$contact= $commonGroundService->getCreateResource($contact, 'https://cc.larping.online/people'); 
+    		
+    		$order['contact'] = $contact['@id'];    		  		
+    		
+    		// order updaten    		
+    		$order = $commonGroundService->getUpdateResource($order); 
     		
     		// order naar bc sturen
     		
-    		// gebruikerdoorsturen naar terug gegeven responce
+    		$invoice= $commonGroundService->getCreateResource($order, 'https://bc.larping.online/invoice'); 
+    		$session->set('invoice',$invoice['@id']);
+    		
+    		// gebruikerdoorsturen naar terug gegeven responce    		
+    		return $this->redirect($invoice['paymentUrl']);
     	}
 
-    	return ['organisations'=>$organizations,'groups'=>$groups, $this->redirect('producten/betalen')];
+    	return ['organisations'=>$organizations,'groups'=>$groups,'order'=>$order,$this->redirect('producten/betalen')];
     }
 
     /**
-     * @Route("/betalen/bevestiging")
+     * @Route("/betalen/bevestiging/{uuid}")
      * @Template
      */
-	public function bevestigingAction(Request $request, CommonGroundService $commonGroundService)
+    public function bevestigingAction(Session $session, Request $request, CommonGroundService $commonGroundService, $uuid)
     {
+    	// Wederom wat doen organizations en groups hier
     	$organizations = $commonGroundService->getResourceList('https://cc.zaakonline.nl/organizations',["name"=>"fc"]);
     	$groups = $commonGroundService->getResourceList('https://pdc.larping.online/groups',["sourceOrganization"=>"816802828"]);
     	
     	// Factuur ophalen aan de hand van id
+    	$invoice = $commonGroundService->getResource('https://bc.larping.online/invoices/'.$uuid);
+    	
+    	// We willen voorkomen dat je via deze route elke factuur kan opvragen
+    	if($invoice['@id'] != $session->get('invoice')){
+    		// Throw auth error
+    	}
     	
     	// info renderen 
+    	$template = $commonGroundService->getResource('https://wrc.larping.online/templates/??????/render',["invoice"=>$invoice]);
     	
     	// mail versturen
 
-    	return ['organisations'=>$organizations,'groups'=>$groups, $this->redirect('producten/betalen/bevestiging')];
+    	return ['organisations'=>$organizations,'groups'=>$groups,'invoice'=>$invoice, $this->redirect('producten/betalen/bevestiging')];
     }
 }
