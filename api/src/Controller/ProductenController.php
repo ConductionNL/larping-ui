652b2a884b48b94e971fbe562f8f22a4e80014ac
Barry <barrybrands02@hotmail.com>
2020-02-19 17:00:01 +0100

    Producten pagina verbeterd

--- api/src/Controller/ProductenController.php
+++ api/src/Controller/ProductenController.php
@@ -28,16 +28,18 @@ class ProductenController extends AbstractController
     	$organizations = $commonGroundService->getResourceList('https://cc.zaakonline.nl/organizations',["name"=>"fc"]);
     	$groups = $commonGroundService->getResourceList('https://pdc.larping.online/groups',["sourceOrganization"=>"816802828"]);
     	$products = $commonGroundService->getResourceList('https://pdc.larping.online/products?sourceOrganization=816802828');
-    	
+
     	// Kijken of het formulier is getriggerd
     	if ($request->isMethod('POST')){
     		// kijken of er in de sessie al een order zit, zo nee order aan maken
-    		
+            $session = $request->getSession();
+            $session->get()
     		// order regel aanmaken op order met de gevraagde gegevens
-    		
+
+
     		// flashban zetten met eindresultaat
     	}
-    	
+
     	return ['organisations'=>$organizations,'groups'=>$groups,'products'=> $products, $this->redirect('producten')];
     }
 
@@ -49,18 +51,17 @@ class ProductenController extends AbstractController
     {
     	$organizations = $commonGroundService->getResourceList('https://cc.zaakonline.nl/organizations',["name"=>"fc"]);
     	$groups = $commonGroundService->getResourceList('https://pdc.larping.online/groups',["sourceOrganization"=>"816802828"]);
-    	
+
     	// Kijken of het formulier is getriggerd
-    	if ($request->isMethod('POST')){
+    	if($request->isMethod('POST')){
     		// contact persoon aanmaken op order
-    		
+
     		// order updaten
-    		
+
     		// order naar bc sturen
-    		
+
     		// gebruikerdoorsturen naar terug gegeven responce
     	}
-
     	return ['organisations'=>$organizations,'groups'=>$groups, $this->redirect('producten/betalen')];
     }
 
@@ -72,11 +73,11 @@ class ProductenController extends AbstractController
     {
     	$organizations = $commonGroundService->getResourceList('https://cc.zaakonline.nl/organizations',["name"=>"fc"]);
     	$groups = $commonGroundService->getResourceList('https://pdc.larping.online/groups',["sourceOrganization"=>"816802828"]);
-    	
+
     	// Factuur ophalen aan de hand van id
-    	
-    	// info renderen 
-    	
+
+    	// info renderen
+
     	// mail versturen
 
     	return ['organisations'=>$organizations,'groups'=>$groups, $this->redirect('producten/betalen/bevestiging')];
