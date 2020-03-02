a43b015a8febd4f69186d87014793f7e47ea0195
Ruben van der Linde <ruben@conduction.nl>
2020-03-02 14:03:10 +0100

    Development on user authenticators

--- api/src/Controller/UserController.php
+++ api/src/Controller/UserController.php
@@ -10,6 +10,9 @@ use Doctrine\ORM\EntityManagerInterface;
 use GuzzleHttp\Client;
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use App\Service\CommonGroundService;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 /**
  * Class UserController
@@ -18,35 +21,29 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
  */
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/login")
+	/**
+	 * @Route("/login")
      * @Template
-     */
-
-    public function loginAction(Request $request, EntityManagerInterface $em)
-    {
-    	// Kijken of het formulier is getriggerd
-    	if ($request->isMethod('POST')) {
-    		
-    		// Lets check on required values
-    		$requiredValues = ['"username"','password'];
-    		$error = false;
-    		foreach($requiredValues as $requiredValue){
-    			if(!$request->request->get($requiredValue)|| $request->request->get($requiredValue) == null){
-    				$this->addFlash('danger', $requiredValue.' is a required value');
-    				$error = true;
-    			}
-    		}
-    	}	
-    		
-        return [];
-    }
+	 */
+	public function login(AuthenticationUtils $authenticationUtils)
+	{
+		// get the login error if there is one
+		$error = $authenticationUtils->getLastAuthenticationError();
+		// last username entered by the user
+		$lastUsername = $authenticationUtils->getLastUsername();
+		
+		if($error){
+			$this->addFlash('danger', $error);
+		}
+		
+		return [];
+	}
 
     /**
      * @Route("/register")
      * @Template
      */
-    public function registerAction(Request $request, EntityManagerInterface $em)
+	public function registerAction(Request $request, CommonGroundService $commonGroundService, ParameterBagInterface $params)
     {
     	// Kijken of het formulier is getriggerd
     	if ($request->isMethod('POST')) {
@@ -62,18 +59,41 @@ class UserController extends AbstractController
     			}
     			
     			
+    			if($request->request->get('password') != $request->request->get('password2')){
+    				$this->addFlash('danger','Password and repeat password do not match');
+    				$error = true;
+    			}
+    			
+    			
+    			$users = $commonGroundService->getResourceList($params->get('auth_provider_user').'/users',["username"->$credentials["username"]]);
+    			$users = $users["hydra:member"];
+    			
+    			if(count($users) >= 1){
+    				$this->addFlash('danger','Username is already taken');
+    				$error = true;
+    			}
+    			
+    			if($error){
+    				return [];
+    			}
+    			
+    			
+    			$application = $commonGroundService->getApplication();
+    			
     			// contact persoon aanmaken op order
     			$contact['givenName'] = $request->request->get('givenName');
     			$contact['familyName'] = $request->request->get('familyName');    			
     			$contact['emails'] = [];
     			$contact['emails'][] = ["name" => "primary", "email" => $request->request->get('username')];
-    			$contact = $commonGroundService->createResource($contact, 'https://cc.larping.eu/people');
+    			//$contact['organization'] = 'https://wrc.larping.eu'.$application['organization']['@id'];
+    			$contact = $commonGroundService->createResource($contact, 'https://cc.larping.eu/people'); /*@todo awfulle specific */
     			
-    			// 
+    			//  Create the uses
     			$user = [];
     			$user['username'] =  $request->request->get('username');
     			$user['password'] =  $request->request->get('password');
-    			$user['contact'] = $contact['@id'];
+    			$user['organization'] = 'https://wrc.larping.eu'.$application['organization']['@id'];
+    			$user['perosn'] = $contact['@id']; 
     		}
     	}	
     	
@@ -85,7 +105,7 @@ class UserController extends AbstractController
      * @Route("/register-confirm")
      * @Template
      */
-    public function confirmAction(Request $request, EntityManagerInterface $em)
+    public function confirmAction(Request $request)
     {
         return [];
     }
@@ -117,7 +137,7 @@ class UserController extends AbstractController
      * @Route("profile")
      * @Template
      */
-    public function profileAction(Request $request, EntityManagerInterface $em)
+    public function profileAction(Request $request)
     {
         return [];
     }
@@ -126,7 +146,7 @@ class UserController extends AbstractController
      * @Route("history")
      * @Template
      */
-    public function historyAction(Request $request, EntityManagerInterface $em)
+    public function historyAction(Request $request)
     {
         return[];
     }
@@ -135,7 +155,7 @@ class UserController extends AbstractController
      * @Route("settings")
      * @Template
      */
-    public function settingsAction(Request $request, EntityManagerInterface $em)
+    public function settingsAction(Request $request)
     {
         return[];
     }
